0b913a826c6e8f2fef47459a19baa03c87f173d8
Samy Pessé <samypesse@gmail.com>
2015-04-04 17:58:15 +0200

    Add view to render template

--- tests/index.js
+++ tests/index.js
@@ -13,6 +13,27 @@ describe('View', function() {
         v.appendTo($body);
 
         expect($("body span").length).to.equal(1);
+        v.remove();
+    });
+
+});
+
+describe('View.Template', function() {
+    var $body = $("body");
+
+    it('should correctly render a template', function() {
+        var MyView = View.Template.extend({
+            template: "Hello <b><%- message %></b>"
+        });
+
+        var v = new MyView({
+            message: "World"
+        });
+        v.appendTo($body);
+        v.update();
+
+        expect(v.html()).to.equal("Hello <b>World</b>");
+        v.remove();
     });
 
 });
